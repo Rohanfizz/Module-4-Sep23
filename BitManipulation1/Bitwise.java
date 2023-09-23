00427919f1b07c3736b6ff2f139cd2ba5a0faa5a
Rohanfizz <rohans0501@gmail.com>
2023-09-23 11:57:58 +0530

    BM1

---
+++ BitManipulation1/Bitwise.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Bitwise {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    System.out.println(10 & 12);
+    System.out.println(10 | 12);
+    System.out.println(10 ^ 12);
+    System.out.println(11 >> 3); //11/2 => 5/2 => 2/2 => 1
+    scn.close();
+  }
+}
---
+++ BitManipulation1/CheckIfKthBitIsSet.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int K=sc.nextInt();
+        
+
+        Solution obj=new Solution();
+        Boolean ans=obj.solve(N,K);
+
+        if(ans==true)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+        
+    }
+}
+
+class Solution{
+    
+    public static Boolean solve(int n,int k){
+        // Your code here
+		int bitMask = 1 << k;
+		if( (n & bitMask) != 0) return true;
+		return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
